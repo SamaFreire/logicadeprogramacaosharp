e106b2b6d6ca47cf5cd15660a549a34cb9570a5f
ELISSAMA FREIRE PENA <elissama.fpena@senacsp.edu.br>
2025-01-29 17:32:18 -0300

    Maratona de Exercicios

---
+++ maratonadeexercicio01/maratonadeexercicio01/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeclaracaodeVariaveiseConstantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercicio 1
+            int idade = 25;
+            const double PI = 3.14159;
+            Console.WriteLine($"idade: {idade}");
+            Console.WriteLine($"PI: {PI}");
+
+
+
+
+            //Exercicio 2
+            string nome = "Maria";
+            const int numeroMaximo = 100;
+
+            Console.WriteLine($"Nome: {nome}");
+
+            if (numeroMaximo == 100)
+            {
+                Console.WriteLine($"Numero Maximo: {numeroMaximo}");
+            }
+            
+
+
+
+
+
+            Console.ReadLine();
+        }
+     
+    }
+}
+ 
